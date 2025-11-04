045d00f4765f0edbebf452125d6f4915fdf9fba7
Josiah44123 <163369246+Josiah44123@users.noreply.github.com>
2025-11-04 16:10:43 +0800

    d

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
 public static void main(String[] args) {
     System.out.println("Hello, World!");
-    switch (
-    ) {
+    System.out.println("Pick a number between 1 and 3:");
+    int
+    switch () {
         case :
             
             break;
